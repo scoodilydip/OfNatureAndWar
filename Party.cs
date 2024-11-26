74c8666aa2cf85f0346897afdb7927d5e1b29fd8
scoodilydip <owengepeel@gmail.com>
2024-11-25 19:15:52 -0800

    Add files via upload

---
+++ Party.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Party : MonoBehaviour
+{
+    public List<GameObject> partyMembers = new List<GameObject>();
+}
